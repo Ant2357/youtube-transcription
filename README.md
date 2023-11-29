05e61f998ec7ec6dfcb56bfff032103d064df7f0
Ant2357 <ryu4979@gmail.com>
2023-11-29 23:52:55 +0900

    feat: init

---
+++ README.md
@@ -0,0 +1,2 @@
+# youtube-transcription
+YouTube の動画を文章として出力する私用コード
---
+++ main.py
@@ -0,0 +1,29 @@
+import whisper
+from yt_dlp import YoutubeDL
+
+model = whisper.load_model("small")
+
+# yt_dlp のオプション
+youtube_urls = ["https://www.youtube.com/watch?v=Aqfd-F0uACw"] 
+ydl_opts = {
+    "format": "mp3/bestaudio/best",
+    "outtmpl": "audio",
+    "postprocessors": [
+        {
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "mp3",
+        }
+    ]
+}
+
+# mp3 の形式でダウンロード
+with YoutubeDL(ydl_opts) as ydl:
+    ydl.download(youtube_urls)
+
+# 文字起こし
+result = model.transcribe("audio.mp3")
+print(result["text"])
+
+# 歌詞ファイルを出力する
+with open("output.txt", "w") as f:
+    f.write(result["text"])
